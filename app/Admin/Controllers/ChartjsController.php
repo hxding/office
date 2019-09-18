7e0d73b680fdb9bdd020d06db626ce391084b1d7
unknown <Docas@CMT-EXTVDI-O143.cmtspace.com>
2019-09-18 10:36:18 +0800

    报表

--- app/Admin/Controllers/ChartjsController.php
+++ app/Admin/Controllers/ChartjsController.php
@@ -18,6 +18,7 @@ class ChartjsController extends Controller
 		$json = '[{"count_date":"03-28","fans_num":5906,"article_num":363,"forward_num":27928,"comment_num":9123,"like_num":35632},{"count_date":"03-29","fans_num":9565,"article_num":361,"forward_num":16755,"comment_num":7193,"like_num":36540}]';
 		
 		$jsonArr = json_decode($json, 1);
+
 		// bindData
 		$head = [
 		    'count_date' => '日期',
@@ -27,14 +28,16 @@ class ChartjsController extends Controller
 		    'forward_num' => '转发',
 		    'like_num' => '点赞',
 		];
-		$echarts = (new Echarts('柱状图', '数据来自新浪云大数据平台'))
+		$echarts = (new Echarts('数据来自新浪云大数据平台')) 
+		            ->setSeriesType('bar')
 		            ->setData($jsonArr)
+
 		            ->bindLegend($head);
 		            
 		return $content
 		            ->header('Echarts demo')
 		            ->description('百度echarts图表展示')
-		            ->body(new Box('折线图', $echarts));
+		            ->body(new Box('柱状图', $echarts));
 
 	}
 
